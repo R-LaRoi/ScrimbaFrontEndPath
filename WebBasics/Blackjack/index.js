fd4bf592af4e557590c7060f5f113aea4587c438
R. LaRoi <are@raewhoo.com>
2023-03-29 22:34:07 +0200

    Password generator

--- WebBasics/Blackjack/index.js
+++ WebBasics/Blackjack/index.js
@@ -1,32 +1,33 @@
+let sum = 0;
+let cards = [];
+let message = "";
+let isPlaying = false;
 let firstCard = getRandomCard();
 let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
-let message = "";
-let isPlaying = true;
-
 let playMessage = document.getElementById("play-message");
-console.log(playMessage);
-
-// let sumDisplay = (document.getElementById("display-sum").textContent =
-//   "sum: " + sum);
-
 let sumDisplay = document.querySelector("#display-sum");
-sumDisplay.textContent = "Sum:" + sum;
-
 let showCards = document.querySelector("#show-cards");
+let hasBlackjack = false;
 
-// showCards.innerText = " Cards: " + firstCard + " - " + secondCard;
+let playerInfo = {
+  playerName: "Wandile",
+  chips: "$2700",
+};
+
 function getRandomCard() {
-  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  let randomNumber = Math.floor(Math.random() * 12) + 1;
   if (randomNumber > 10) {
-    return 10;
+    return 7;
   } else if (randomNumber === 1) {
     return 11;
   } else return randomNumber;
 }
 
 function newGame() {
+  sum = firstCard + secondCard;
+  isPlaying = true;
+  showCards.innerText = firstCard + " " + secondCard;
+  sumDisplay.innerText = firstCard + secondCard;
   playGame();
 }
 
@@ -35,23 +36,48 @@ function playGame() {
     showCards.textContent += cards[i] + " ";
   }
 
+  sumDisplay.textContent = "Sum:" + sum;
   if (sum <= 20) {
-    message = "You are a Blackjack Winner!";
-  } else if (sum === 21) {
     message = "Would you like to draw a new card?";
-  } else {
+  } else if (sum === 21) {
+    message = "You are a Blackjack Winner!";
+    hasBlackjack = true;
+  } else if (sum > 21) {
+    message = "You are out of the game!";
     isPlaying = false;
-    message = "Draw another card.";
   }
 
   playMessage.innerText = message;
 }
 
 function drawCard() {
-  let newCard = getRandomCard();
-  sum += newCard;
-
-  cards.push(newCard);
-  console.log(cards);
-  playGame();
+  if (isPlaying === true && hasBlackjack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    playGame();
+  }
 }
+
+let player = document.querySelector("#player-info");
+player.innerText = playerInfo.playerName + ": " + playerInfo.chips;
+
+// let fruit = ["🍎", "🍊", "🍎", "🍎", "🍊"]
+// let appleShelf = document.getElementById("apple-shelf")
+// let orangeShelf = document.getElementById("orange-shelf")
+
+// // Create a function that puts the apples onto the appleShelf
+// // and the oranges onto the orangeShelf. Use a for loop,
+// // a conditional statement, and the textContent property.
+
+// // for (i = 0; i < fruits.length; i++)
+
+//   function storefruit(){
+//     for (i = 0; i < fruit.length; i++) {
+//     if (fruit[i] === "🍎" ){
+//         appleShelf.innerText += "🍎"
+//     } else if (fruit[i] === "🍊") {
+//       appleShelf.innerText += "🍎";
+//     }
+
+// }}
