0a6817f8f06df50aa871a455917acba908262b51
R. LaRoi <are@raewhoo.com>
2023-04-11 18:05:54 +0200

    JS ICON Functions

--- JavscriptEssentials/Twimba/index.js
+++ JavscriptEssentials/Twimba/index.js
@@ -1,7 +1,7 @@
 const tweetsData = [
   {
     handle: `@TrollBot66756542 💎`,
-    profilePic: `images/troll.jpg`,
+    profilePic: `avatar.png`,
     likes: 27,
     retweets: 10,
     tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -61,7 +61,7 @@ let tweetBtn = document.getElementById("tweet-btn");
 let tweetInput = document.getElementById("tweet-input");
 let tweetFeed = document.getElementById("tweet-feed");
 
-tweetBtn.addEventListener("click", () => {
+tweetBtn.addEventListener("click", function () {
   console.log(tweetInput.value);
 });
 
@@ -69,25 +69,83 @@ document.addEventListener("click", function (e) {
   if (e.target.dataset.heart) {
     handleLikeClick(e.target.dataset.heart);
   }
+
   if (e.target.dataset.dots) {
-    console.log(e.target.dataset.dots);
+    handleReplies(e.target.dataset.dots);
   }
   if (e.target.dataset.retweet) {
-    console.log(e.target.dataset.retweet);
+    handleRetweet(e.target.dataset.retweet);
   }
 });
 
 function handleLikeClick(tweetId) {
-  console.log(tweetId);
+  const targetTweetObj = tweetsData.filter(function (tweet) {
+    return tweet.uuid === tweetId;
+  })[0];
+
+  if (targetTweetObj.isLiked) {
+    targetTweetObj.likes--;
+  }
+  targetTweetObj.isLiked = !targetTweetObj.isLiked;
+
+  renderFeed();
+}
+
+function handleRetweet(tweetId) {
+  const retweetObj = tweetsData.filter(function (retweet) {
+    return retweet.uuid === tweetId;
+  })[0];
+
+  if (retweetObj.isRetweeted) {
+    retweetObj.retweets--;
+  } else {
+    retweetObj.retweets++;
+  }
+  retweetObj.isRetweeted = !retweetObj.isRetweeted;
+
+  renderFeed();
+}
+
+function handleReplies(tweetId) {
+  const repliesObj = tweetsData.filter(function (replies) {
+    return replies.uuid === tweetId;
+  })[0];
+
+  if (repliesObj.replies.length > 0) {
+    console.log(repliesObj.uuid);
+
+    repliesObj.replies.forEach(function () {
+      repliesHtml += `<div class="tweet-reply">
+    <div class="tweet-inner">
+        <img src=${profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${handle}</p>
+                <p class="tweet-text">${tweetText}</p>
+            </div>
+        </div>
+</div>`;
+    });
+  }
 }
 
 function getFeedHTML() {
   let HTMLfeed = ``;
+
   tweetsData.forEach(function (tweet) {
+    let likeHeart = "";
+    let iconShare = "";
+
+    if (tweet.isLiked) {
+      likeHeart = "liked";
+    }
+    if (tweet.isRetweeted) {
+      iconShare = "retweeted";
+    }
+
     HTMLfeed += `
     <div class="tweet">
     <div class="tweet-inner">
-        <img src="${tweet.profilePic}" class="profile-pic">
+        <img src="${tweet.profilePic}  class="profile-pic">
         <div>
             <p class="handle">${tweet.handle}</p>
             <p class="tweet-text">${tweet.tweetText}</p>
@@ -97,16 +155,18 @@ function getFeedHTML() {
                     ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                <i class="fa-solid fa-heart" data-heart="${tweet.uuid}"></i>
+                <i class="fa-solid fa-heart ${likeHeart}" data-heart="${tweet.uuid}"></i>
                     ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
-             <i class="fa-solid fa-retweet" data-retweet=${tweet.uuid}></i>
+             <i class="fa-solid fa-retweet ${iconShare}" data-retweet=${tweet.uuid}></i>
                     ${tweet.retweets}
                 </span>
             </div>   
         </div>            
     </div>
+     <div id="replies-${tweet.uuid}">
+        ${repliesHtml}
 </div>`;
   });
   return HTMLfeed;
